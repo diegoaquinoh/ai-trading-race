b5922f4c1c3a4b188c082d7ac7cc6c6e3320f55e
diegoaquinoh <diegoaquinoh@gmail.com>
2026-02-17 17:25:59 +0100

    fix llama call

--- AiTradingRace.Infrastructure/Migrations/20260217162436_AddGpt4oAgent.Designer.cs
+++ AiTradingRace.Infrastructure/Migrations/20260217162436_AddGpt4oAgent.Designer.cs
@@ -4,6 +4,7 @@ using AiTradingRace.Infrastructure.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AiTradingRace.Infrastructure.Migrations
 {
     [DbContext(typeof(TradingDbContext))]
-    partial class TradingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260217162436_AddGpt4oAgent")]
+    partial class AddGpt4oAgent
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
